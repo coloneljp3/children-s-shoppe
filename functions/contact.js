e2f0ffa1f59438a0b5708c3a8e2e044add120d37
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2024-09-27 12:35:29 -0500

    Create contact.js

---
+++ functions/contact.js
@@ -0,0 +1,109 @@
+'use strict'
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+const serverless = require('serverless-http');
+const router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use('/',(req,res)=>{
+res.send(`
+<!DOCTYPE html>
+<html>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Baskervville+SC&display=swap" rel="stylesheet">
+<link rel = "stylesheet" href = "/styles.css">
+<header>
+<meta></meta>
+<nav id = "top-of-page-nav-bar">
+<form class = "top-of-page-nav-bar-form" method = "POST"><button class = "top-of-page-nav-bar-form-button" type = "submit">Home</button></form>
+<form class = "top-of-page-nav-bar-form" method = "POST"><button class = "top-of-page-nav-bar-form-button" type = "submit">Services</button></form>
+<form class = "top-of-page-nav-bar-form" method = "POST"><button class = "top-of-page-nav-bar-form-button" type = "submit">Contact</button></form>
+<form class = "top-of-page-nav-bar-form" method = "POST"><button class = "top-of-page-nav-bar-form-button" type = "submit">Hiring</button></form>
+
+</nav>
+
+</header>
+
+<body>
+
+<div class = "body-div" id = "body-div-1">
+
+<h1 id = "body-div-1-heading-1"></h1>
+</div>
+
+<div id = "body-div-2" class = "body-div">
+<h2 class = "body-div-h2"></h2>
+<p class = "body-div-p"></p>
+</div>
+<div id = "body-div-3" class = "body-div">
+<h2 class = "body-div-h2"></h2>
+<p class = "body-div-p"></p>
+</div>
+<div id = "body-div-4"  class = "body-div">
+<h2 class = "body-div-h2"></h2>
+<p class = "body-div-p"></p>
+</div>
+
+
+</body>
+
+<footer id = "footer">
+<span id = "footer-span-1">
+<section class = "footer-span-section" id = "footer-span-section-1"></section>
+<section class = "footer-span-section" id = "footer-span-section-2"></section>
+<section class = "footer-span-section" id = "footer-span-section-3"></section>
+<section class = "footer-span-section" id = "footer-span-section-4"></section>
+
+</span>
+
+<span id = "footer-span-2">
+<section class = "footer-span-section" id = "footer-span2-section-1"></section>
+<section class = "footer-span-section" id = "footer-span2-section-2"></section>
+<section class = "footer-span-section" id = "footer-span2-section-3"></section>
+<section class = "footer-span-section" id = "footer-span2-section-4"></section>
+
+</span>
+
+<span id = "footer-span-3">
+<section class = "footer-span-section" id = "footer-span3-section-1"></section>
+<section class = "footer-span-section" id = "footer-span3-section-2"></section>
+<section class = "footer-span-section" id = "footer-span3-section-3"></section>
+<section class = "footer-span-section" id = "footer-span3-section-4"></section>
+
+</span>
+
+
+</footer>
+
+</html>
+
+
+`)
+
+  
+})
+
+//router.post('/',(req,res)=>{
+//var user = req.body.user;
+//var email_user = req.body.email_user;
+//var email_psw = req.body.email_psw;
+//if(user == "undefined"){
+//res.send()
+  
+//}
+  //else{
+//res.send("")
+    
+  //}
+  
+//})
+
+//router.get('/',(req,res)=>{})
+
+app.use('/.netlify/functions/contact',router)
+
+module.exports.handler = serverless(app);
