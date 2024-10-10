883011b2b82850843acf0a0fa59ceaf6056f1b31
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2024-10-10 00:18:41 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -39,12 +39,16 @@ if(elem.style.borderBottomColor == color_1){
 elem.style.borderBottomColor = color_2;}else{elem.style.borderBottomColor = color_1;}}
 
 function dropDown(classname){
-var dropDown = document.getElementsByClassName(classname);
+var dropDown = Object.keys(document.getElementsByClassName(classname));
 for(let i of dropDown){
   if(i.style.height = '100%'){
-i.style.height = '0px';    
+i.style.height = '0px';
+i.style.width = '0px';
+i.style.visibility = "hidden";
 }
   else{
+    i.style.visibility = "visible"
+    i.style.width = 100%;
     i.style.height = 100%;
   }
   }
