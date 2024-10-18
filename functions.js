d8c9293abce8e530d47d3dd7ca7f8f475a7c9815
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2024-10-18 14:02:51 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -83,6 +83,7 @@ document.getElementById('product-selection').innerHTML = xml.responseText;
 };
 xml.open('GET','https://chidrensshoppe.netlify.app/products.xml','true');
     xml.setRequestHeader('Access-Control-Allow-Origin','*');
+  xml.setRequestHeader('Access-Control-Request-Headers','*')
 
 xml.send()
   
