82687913d7a45ca93e062acb997f9d7701c947ad
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2024-11-03 17:30:39 -0600

    Update home.js

--- functions/home.js
+++ functions/home.js
@@ -295,7 +295,7 @@ width.style.width = '0px'}
 <div id = "post-header-body-div-1" class = "body-div">
 
 
-<h1 id = "main-page-heading" onload = "this.style.visibility = 'visible'">The Children's Shoppe</h1>
+<div style = "width:100%;background-color:black"><h1 id = "main-page-heading" onload = "this.style.visibility = 'visible'">The Children's Shoppe</h1></div>
 <div style = "width:100%">
 <h1 style = "text-align:center;height:50%;width:50%;z-index:1;color:white">Browse Our Store Online Or In Person!</h1>
 <img src = "https://www.consignkoolkids.com/img/slideClothesEdge.jpg" id = "image-slider" style = "height:100%;opacity:50%" ></img>
