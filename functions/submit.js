9889ec516681958dec09cfc029a53502ee8bea3c
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2024-12-04 09:26:18 -0600

    Update submit.js

--- functions/submit.js
+++ functions/submit.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use('/',(req,res)=>{
 let mail = nodemailer.createTransport({
-	host:smtp.gmail.com,
+	host:'smtp.gmail.com',
 	port:587,
 	secure:false,
 	auth:{
