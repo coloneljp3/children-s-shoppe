6fddde36ad62be04574563c92d542ef1da1e87e3
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2024-12-06 01:42:12 -0600

    Update home.js

--- functions/home.js
+++ functions/home.js
@@ -312,12 +312,12 @@ width.style.width = '0px'}
 
 </button>
 <button id = "arrow_2" onclick = "shiftImage(['https://scontent-dfw5-2.xx.fbcdn.net/v/t39.30808-6/326706496_3310115642633019_2159792958870315398_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=mQtcmYTH5HIQ7kNvgGbkPeO&_nc_zt=23&_nc_ht=scontent-dfw5-2.xx&_nc_gid=AmqjLffJszvv4kLMv8bD48C&oh=00_AYBA6nn84jTfbhrh8HRPvdmTRpsK68ohphqmSMMRGO48gQ&oe=672CD2EB'
-,'https://childrensshoppe.netlify.app/pic_1.jpg','https://childrensshoppe.netlify.app/pic_2.jpg','https://childrensshoppe.netlify.app/pic_3.jpg','https://childrensshoppe.netlify.app/pic_4.jpg'],'image-slider-image','arrow_2')">
+,'https://childrensshoppe.netlify.app/pic_1.jpg','https://childrensshoppe.netlify.app/pic_2.jpg','https://childrensshoppe.netlify.app/pic_3.jpg','https://childrensshoppe.netlify.app/pic_4.jpg'],'image-slider','arrow_2')">
 >
 
 </button>
 <button id = "call-to-action" style = "border-width:5px;border-color:white;border-radius:25px;font-family:Raleway;text-align:center;position:absolute;font-size:40px;width:700px;left:12.5%;top:75%;z-index:1;color:white;margin:auto;background-color:transparent;border-style:none" onmouseover="propChange('call-to-action','transition', 'border-style 1s')propChange('call-to-action','borderStyle','solid');" onmouseleave="propChange('call-to-action','borderStyle','none');">Browse Our Store Online Or In Person!</button>
-<img src = "https://www.consignkoolkids.com/img/slideClothesEdge.jpg" id = "image-slider-image" style = "filter:grayscale(40%);height:100%;opacity:80%" ></img>
+<img src = "https://www.consignkoolkids.com/img/slideClothesEdge.jpg" id = "image-slider" style = "filter:grayscale(40%);height:100%;opacity:80%" ></img>
 <svg style = "text-align:center;margin:auto"><circle cx = "20" cy = "20" r = "5" stroke = "black"></circle>
 <circle cx = "40" cy = "20" r = "5" stroke = "black"></circle>
 <circle cx = "60" cy = "20" r = "5" stroke = "black"></circle>
