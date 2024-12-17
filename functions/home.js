dc116d959d660665faf12b7cbaf35bccf5164e4a
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2024-12-16 23:58:29 -0600

    Update home.js

--- functions/home.js
+++ functions/home.js
@@ -84,7 +84,7 @@ setTimeout(()=>{image.style.opacity = 0%;image.style.opacity = 100%;image.src = 
 
 </svg>
 
-<div id = "menu-bar" style = "font-size:0px;position:absolute;z-index:1;width:0px;height:0px;border-right-style:solid;border-right-width:.01px;display:inline-block"><button type = "submit" class = "menu-bar-options">Contact</button>
+<div id = "menu-bar" style = "font-size:0px;position:fixed;z-index:1;width:0px;height:0px;border-right-style:solid;border-right-width:.01px;display:inline-block"><button type = "submit" class = "menu-bar-options">Contact</button>
 <button type = "submit" class = "menu-bar-options">Contact</button>
 <button type = "submit" class = "menu-bar-options">Contact</button>
 <button type = "submit" class = "menu-bar-options">Contact</button>
