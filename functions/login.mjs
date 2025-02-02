b1bb5b3f8f4cf9a4195dbb8e3b065a4c402ef0aa
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-02-01 22:26:18 -0600

    Update login.mjs

--- functions/login.mjs
+++ functions/login.mjs
@@ -1,6 +1,6 @@
 'use strict'
 
-//var awsSdk = require('@aws-sdk/dsql-signer')
+var awsSdk = require('@aws-sdk/dsql-signer')
 var assert = require('node:assert')
 var pg = require('pg')
 const mysql = require('mysql2');
