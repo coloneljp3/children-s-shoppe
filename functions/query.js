36a38fa3ca94c801062174b256fb54837334469b
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-02-06 00:48:42 -0600

    Update query.js

--- functions/query.js
+++ functions/query.js
@@ -1,7 +1,7 @@
 var aws = require('@aws-cdk/aws-lambda-nodejs');
 const handler = new aws.NodejsFunction(this, 'DSQLHandler', {
   handler: 'handler',
-  entry: '/.netlify/functions/login',
+  entry: __dirname + '/login.js',
   memorySize: 1024,
   bundling: {
     bundleAwsSDK: true,
