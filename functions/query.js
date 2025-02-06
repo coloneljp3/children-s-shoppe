de58c32fe87b37828a43fdb24d14207a345cebf5
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-02-06 01:32:32 -0600

    Update query.js

--- functions/query.js
+++ functions/query.js
@@ -1,7 +1,7 @@
 var aws = require('@aws-cdk/aws-lambda-nodejs');
 const handler = new aws.NodejsFunction(this, 'DSQLHandler', {
   handler: 'handler',
-  entry: __dirname + '/login.js',
+  entry:'/var/task/login.js',
   memorySize: 1024,
   bundling: {
     bundleAwsSDK: true,
