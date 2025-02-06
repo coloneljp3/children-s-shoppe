376774c4805307b91446b5a7a1752e3e5c9970da
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-02-06 12:32:16 -0600

    Update quer.js

--- functions/quer.js
+++ functions/quer.js
@@ -1,12 +1,4 @@
-var express = require('express')
-var app = express()
-var serverless = require('serverless-http')
-var bodyParser = require('body-parser')
-var router = express.Router()
-var aws = require('@aws-cdk/aws-lambda-nodejs');
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.get('/',(req,res)=>{
+
 const handler = new aws.NodejsFunction(this, 'DSQLHandler', {
   handler: 'handler',
   code:"",
@@ -24,7 +16,3 @@ handler.addToRolePolicy(
     resources: ['*']
   })
 );
-
-});
-app.use('/.netlify/functions/quer',router)
-module.exports.handler = serverless(app)
