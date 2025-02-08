6a59ddf240d8e45dc03acdc1fc94780d66b88c22
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-02-08 17:03:53 -0600

    Update quer.js

--- functions/quer.js
+++ functions/quer.js
@@ -1,15 +1,10 @@
 var lambda = require('aws-cdk-lib')
+var run = require('aws-cdk-lib/aws-lambda')
 var path = require('path')
-var express = require('express');
-var app = express();
-var router = express.Router()
-var serverless = require('serverless-http')
-app.get('/',(req,res)=>{
-res.send(lambda.Runtime)
-  
-})
+
+
 const handler = new lambda.NodejsFunction(this, 'DSQLHandler', {
- // runtime: lambda.Runtime.NODEJS_20_X,
+  runtime: run.Runtime.NODEJS_20_X,
   handler: 'handler',
   entry:path.resolve("childrensshoppe.netlify.app/.netlify/functions","/login.js"),
   memorySize: 1024,
@@ -25,6 +20,3 @@ handler.addToRolePolicy(
     resources: ['*']
   })
 );
-app.use('/.netlify/functions/quer',router)
-
-module.exports.handler = serverless(app)
