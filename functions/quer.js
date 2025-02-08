569cb312c74ea82e587a20278d691a5b3606af85
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-02-08 16:31:35 -0600

    Update quer.js

--- functions/quer.js
+++ functions/quer.js
@@ -1,6 +1,13 @@
 var lambda = require('aws-cdk-lib')
 var path = require('path')
-
+var express = require('express');
+var app = express();
+var router = express.Router()
+var serverless = require('serverless-http')
+app.post('/',(req,res)=>{
+res.send(lambda.Runtime)
+  
+})
 const handler = new lambda.NodejsFunction(this, 'DSQLHandler', {
   runtime: lambda.Runtime.NODEJS_20_X,
   handler: 'handler',
@@ -18,3 +25,6 @@ handler.addToRolePolicy(
     resources: ['*']
   })
 );
+app.use('/.netlify/functions/quer',router)
+
+module.exports.handler = serverless(app)
